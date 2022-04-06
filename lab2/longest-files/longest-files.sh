a7b46209c7e46e19e7efa7fd19ff27ad336a6cbb
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-04-06 15:43:50 +0200

    fixed argument checking in longest-files

--- lab2/longest-files/longest-files.sh
+++ lab2/longest-files/longest-files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# check if user gave an argument
+if [[ "$#" -ne 1 ]]; then
+  echo "One argument required (number of lines)!"
+  exit 1
+fi
+
 paths -s -R | grep ".go" | process --project=1 --delimiter=" " | sort -nrk1 | myHead --lines="$1"
