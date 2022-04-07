5f2dd7e5f81abe5fee08df2a8afc7246c55fdd6d
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-04-07 02:10:38 +0200

    death ratio in any continent

---
+++ lab3/death-ratio.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# check if user passed 2 arguments
+if [[ "$#" -ne 2 ]]; then
+  echo "You have to pass 2 args in order: \"ratio [continent] [year]\""
+  exit 1
+fi
+
+CASES=$(cat covid.tsv.txt |
+  process --delimiter=$'\t' --separator=";" --project=10,3,4 |
+  grep "$1;$2" |
+  process --delimiter=";" --project=2 |
+  aggregate --using=sum --separator="")
+CASES=${CASES//".00"/""}
+
+DEATHS=$(cat covid.tsv.txt |
+  process --delimiter=$'\t' --separator=";" --project=10,3,5 |
+  grep "$1;$2" |
+  process --delimiter=";" --project=2 |
+  aggregate --using=sum --separator="")
+DEATHS=${DEATHS//".00"/""}
+
+if [[ "$CASES" -eq 0 ]]; then
+  echo "There is not enought data to calculate death/cases ratio!"
+  exit 1
+fi
+
+printf "Death rate in %s in %s was %.5f%%\n" $1 $2 $((10**6 * DEATHS/CASES))e-6
